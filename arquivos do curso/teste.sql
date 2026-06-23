b7be3a31f9de3634ec948d6fb72c0bfcc3b766d1
Andre Silva <andrezinhoss247@gmail.com>
2026-06-23 01:49:49 -0300

    Finalizando aula15

--- arquivos do curso/teste.sql
+++ arquivos do curso/teste.sql
@@ -1,36 +1,58 @@
-CREATE DATABASE agenda              -- 1. Cria banco de dados
+-- Active: 1781838802528@@127.0.0.1@3306
+CREATE DATABASE agendamento                  -- 1. Cria banco de dados
 DEFAULT CHARACTER SET utf8mb4            -- 2. Padrao universal suporta quase todas as linguas
 DEFAULT COLLATE utf8mb4_unicode_ci;      -- 3. Ignora maiúsculas/minúsculas nas buscas (A = a)
 
 #Usando banco de dados antes de criar as tabelas
-USE agenda;
+USE agendamento;
 
 # Criando uma tabela
 CREATE TABLE Cliente 
 ( 
- idCliente INT PRIMARY KEY AUTO_INCREMENT,  
+ idcliente INT PRIMARY KEY AUTO_INCREMENT,  
  nome VARCHAR(30) NOT NULL,  
  sobrenome VARCHAR(40) NOT NULL
 ); 
 
-CREATE TABLE Agendamento 
+
+
+
+CREATE TABLE Servico 
 ( 
- idAgendamento INT PRIMARY KEY AUTO_INCREMENT,  
- data DATE TIME,  
- servico VARCHAR(30) NOT NULL,  
- nomeDoAtendente VARCHAR(30) NOT NULL 
+ idservico INT PRIMARY KEY AUTO_INCREMENT,  
+ tiposervico VARCHAR(30) NOT NULL,  
+ valor int NOT NULL 
 ); 
 
-CREATE TABLE Solicita 
+CREATE TABLE Agenda 
 ( 
- idCliente INT,  
- idAgendamento INT,  
- dataDaSolicitacao DATE AUTO_INCREMENT,  
+ idcliente INT,  
+ idservico INT,  
+ datadasolicitacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+ datadoservico DATETIME,
  
- PRIMARY KEY (idCliente, idAgendamento),
- FOREIGN KEY (idCliente) REFERENCES Cliente (idCliente),
- FOREIGN KEY (idAgendamento) REFERENCES Agendamento (idAgendamento)
+ PRIMARY KEY (idcliente, idservico),
+ FOREIGN KEY (idcliente) REFERENCES Cliente (idcliente),
+ FOREIGN KEY (idservico) REFERENCES Servico (idservico)
 );
 
+SELECT * FROM Servico;
+SELECT * FROM Agenda;
 
 DROP DATABASE agenda
+
+
+INSERT INTO Cliente VALUES
+(DEFAULT,'Andre', 'Silva'),
+(DEFAULT,'Daiane', 'Santos'),
+(DEFAULT,'Mabelle', 'Souza');
+
+INSERT INTO Servico VALUES
+(DEFAULT,'Corte', '35'),
+(DEFAULT,'Barba', '20'),
+(DEFAULT,'Sobrancelha', '10');
+
+INSERT INTO Agenda VALUES
+('1','2',DEFAULT,'2026-06-24 14:00:18'),
+('3','1',DEFAULT,'2026-06-24 15:00:18'),
+('2','3',DEFAULT,'2026-06-24 16:00:18');
--- aula15_chaves_estrangeiras_e_join.sql
+++ aula15_chaves_estrangeiras_e_join.sql
@@ -12,3 +12,23 @@ SELECT * FROM gafanhotos;
 
 
 UPDATE gafanhotos SET cursopreferido = '5' WHERE id = '2';
+
+# Selecionado coluna das duas tabelas com inner join
+SELECT gafanhotos.nome, cursos.nome, cursos.ano 
+FROM gafanhotos INNER JOIN cursos
+ON cursos.idcursos = gafanhotos.cursopreferido
+ORDER BY cursos.ano;
+
+
+# Adicionado apelido para as tabelas no select
+SELECT g.nome, c.nome, c.ano 
+FROM gafanhotos as g  INNER JOIN cursos as c
+ON c.idcursos = g.cursopreferido
+ORDER BY c.ano;
+
+
+# Selecionando todos da tabela gafanhotos mesmo que nao tem curso preferido, OBS: LEFT 
+SELECT g.nome, c.nome, c.ano 
+FROM gafanhotos as g  LEFT OUTER JOIN cursos as c
+ON c.idcursos = g.cursopreferido
+ORDER BY c.ano;
